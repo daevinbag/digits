b8e1a9c4fae9c76dc03b8f01ead6b2f559f3c67b
daevinbag <daevinbag@gmail.com>
2024-04-01 16:32:04 -1000

    contact form

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
@@ -12,6 +13,9 @@ const Contact = ({ contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <td>
+        <Link to={`/edit/${contact._id}`}>Edit</Link>
+      </td>
     </Card.Body>
   </Card>
 );
@@ -24,7 +28,7 @@ Contact.propTypes = {
     address: PropTypes.string,
     image: PropTypes.string,
     description: PropTypes.string,
-    // _id: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
